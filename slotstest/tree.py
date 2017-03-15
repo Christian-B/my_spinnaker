91c50964bfc254cfd53be9d2b63425581a9d0c77
Christian <brenninc@cs.man.ac.uk>
2017-03-15 16:57:38 +0000

    slots test

--- slotstest/tree.py
+++ slotstest/tree.py
@@ -59,8 +59,48 @@ try:
 except TypeError:
     print "Same slot in two parent"
 
+
+class Twelve(object):
+
+    __slots__ = ("X", "Y")
+
+try:
+    class Thirteen(Ten, Twelve):
+        pass
+
+except TypeError:
+    print "Two different slots"
+
+
+class Fourteen(object):
+
+    __slots__ = ("Bar", "Beta", "X", "Y")
+
+
+try:
+    class Fifeteen(Fourteen, Ten):
+        pass
+except TypeError:
+    print "slots in two parent"
+
+
+class Sixteen(object):
+    pass
+    # NO slots
+
+class Seventeen(Sixteen):
+
+    __slots__ = ("name")
+
+    def __init__(self):
+        self.name = "Paul"
+        self.other = 123
+
 four = Four()
 print four
 
 nine = Nine()
 print nine
+
+seventeen = Seventeen()
+print seventeen.other
