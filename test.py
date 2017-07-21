983c46473edb523080219d7ccd9efc42a88fe627
Christian <brenninc@cs.man.ac.uk>
2017-07-21 14:33:40 +0100

    BUG FIX!

---
+++ test.py
@@ -0,0 +1,6 @@
+import os
+
+asstring = os.environ.get('P7_INTEGRATION_FACTOR', "1")
+print asstring
+p7_integration_factor = float(os.environ.get('P7_INTEGRATION_FACTOR', "1"))
+print p7_integration_factor
