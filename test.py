eb09dec5503b3c6ca4ed018b84a7728a20c915f3
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-01-24 14:47:31 +0000

    flake8

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-import os
+import time
 
-asstring = os.environ.get('P7_INTEGRATION_FACTOR', "1")
-print asstring
-p7_integration_factor = float(os.environ.get('P7_INTEGRATION_FACTOR', "1"))
-print p7_integration_factor
+
+def foo(a, b):
+    c = a - b
+    print c
+
+
+t0 = time.time()
+for i in xrange(10000):
+    foo(6, 10)
+t1 = time.time()
+
+print t1-t0
