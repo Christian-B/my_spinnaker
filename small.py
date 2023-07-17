7845c0b95985ba0abf997d7c6228047fdcf534c4
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-07-17 15:27:00 +0100

    Update small.py

--- small.py
+++ small.py
@@ -1,7 +1,4 @@
 import pyNN.spiNNaker as sim
-# import pyNN.utility.plotting as plot
-# import matplotlib.pyplot as plt
-
 sim.setup(timestep=1.0)
 pop_1 = sim.Population(1, sim.IF_curr_exp(), label="pop_1")
 pop_1.record(["spikes", "v"])
